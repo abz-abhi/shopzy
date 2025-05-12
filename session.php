a806dc11f7089a192347b239d7ab3fcf02c808db
abhishek <abhislinux@gmail.com>
2025-05-12 16:17:14 +0530

    session conditon complited

--- session.php
+++ session.php
@@ -14,17 +14,20 @@ if (isset($_SESSION["user_id"])) {
     $result = mysqli_query($con, "SELECT * FROM `users` WHERE `id`='$session_id'");
     $row_user = mysqli_fetch_assoc($result);
 
+
+
     if (isset($_SESSION['cart'])) {
         $session_cart = $_SESSION['cart'];
         foreach ($session_cart as $key => $value) {
 
-            // $result_cartproduct = mysqli_query($con, "SELECT * FROM product WHERE id='" . $value['id'] . "'");
-            // $row_cartproduct = mysqli_fetch_assoc($result_cartproduct);
-            $addCart = "INSERT `cart` (`user_id`, `pro_id`, `qty`, `price`, `created_on`, `updated_on`)
-            VALUES ('$session_id','" . $value['id'] . "','" . $value['qty'] . "','" . $value['price'] . "','" . date('Y-m-d H:i:s') . "','" . date('Y-m-d H:i:s') . "') ";
-            mysqli_query($con, $addCart);
-        }
+            $selectCart = mysqli_query($con, "SELECT * FROM `cart` WHERE `pro_id` = '" . $value['id'] . "' ");
+            if (mysqli_fetch_assoc($selectCart) < 0) {
 
+                $addCart = "INSERT `cart` (`user_id`, `pro_id`, `qty`, `price`, `created_on`, `updated_on`)
+                                   VALUES ('$session_id','" . $value['id'] . "','" . $value['qty'] . "','" . $value['price'] . "','" . date('Y-m-d H:i:s') . "','" . date('Y-m-d H:i:s') . "') ";
+                mysqli_query($con, $addCart);
+            }
+        }
         unset($_SESSION['cart']);
     }
 } else {
